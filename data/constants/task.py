b009bc42769d180595743c6f238b4d27fe02e653
tnb-oceane <tenebaymarc.oceane@gmail.com>
2025-03-11 23:50:04 +0100

    Modification target_metrics

--- data/constants/task.py
+++ data/constants/task.py
@@ -1,13 +1,12 @@
 class Task(object):
 
-    def __init__(self, id, parents_id, n_instructions, is_assigned=-1, is_done=False):
+    def __init__(self, id, parents_id, n_instructions, is_assigned=-1):
 
         """
         id: int -> Identifiant unique de la tâche.
         parent_id: int -> Liste des identifiants des tâches parentes dont cette tâche dépend.
         n_instructions: int -> Nombre d'instructions à exécuter pour cette tâche.
         is_assigned: int -> Identifiant de la machine à laquelle la tâche est affectée (-1 si elle n'est pas encore assignée).
-        is_done: bool -> Indique si la tâche a été terminée (True) ou non (False).
         start_time: float -> Temps de début d'exécution de la tâche (initialisé à 0).
         end_time: float -> Temps de fin d'exécution de la tâche (initialisé à 0).
         """
@@ -16,7 +15,6 @@ class Task(object):
         self.parents_id = parents_id
         self.n_instructions = n_instructions
         self.is_assigned = is_assigned
-        self.is_done = is_done
         self.start_time = 0
         self.end_time = 0
 
@@ -25,6 +23,5 @@ class Task(object):
             f"parents_id: {self.parents_id}\n"\
             f"n_instructions: {self.n_instructions}\n"\
             f"is_assigned: {self.is_assigned}\n"\
-            f"is_done: {self.is_done}\n"\
             f"self.start_time: {self.start_time}\n"\
             f"self.end_time: {self.end_time}\n"
--- target_metrics.py
+++ target_metrics.py
@@ -1,35 +1,51 @@
-
 import numpy as np
 
-# get the cost of executing a task on a machine
+
 def f_cost(dataset, solution):
-    """Cost target function.
+    """ Fonction objectif de coût.
     Args: 
-        dataset: dataset object
-        solution: binary array of shape (n_tasks, n_machines)
-    Returns: total cost of the solution    
+        dataset : objet dataset
+            Ensemble de données contenant les informations sur les tâches et les machines.
+        solution : tableau binaire de forme (n_tasks, n_machines)
+            Matrice binaire indiquant l'affectation des tâches aux machines.
+    Returns: 
+            Coût total de la solution. 
     """
+    def f_transfer_cost(dataset, task, machine):
+        transfer_cost = 0
+        pred_machines = [dataset.get_task_by_id(id).is_assigned for id in task.parents_id]
+        for i in range(len(pred_machines)):
+            if pred_machines[i] == machine.id:
+                transfer_cost += 0
+            else :
+                transfer_cost += (dataset.data_volume[task.parents_id[i], task.id] / dataset.bandwidth[pred_machines[i], machine.id])*dataset.bandwidth_cost[pred_machines[i], machine.id]
+        return transfer_cost
+    
     cost = 0
     for task in dataset.tasks:
         for machine in dataset.machines:
             if solution[task.id, machine.id] == 1:
                 # Execution cost
-                exec_cost = task.n_instructions / machine.cpu_mips * machine.cpu_cost
+                exec_cost = (task.n_instructions / machine.cpu_mips) * machine.cpu_cost
+                
                 # Communication cost
-                predecessors = [dataset.get_task_by_id(id) for id in task.parents_id]
-                pred_machines = [p.is_assigned for p in predecessors]
-                transfer_cost = sum([dataset.bandwidth_cost[id, machine.id] for id in pred_machines])
+                transfer_cost = f_transfer_cost(dataset,task,machine)
+                
                 # Update total cost
-                cost += exec_cost + transfer_cost
-    
+                cost += exec_cost + transfer_cost  
     return cost
+    
+
 
 def f_makespan(dataset, solution):
     """Makespan target function.
     Args:
-        dataset: dataset object
-        solution: binary array of shape (n_tasks, n_machines)
-    Returns: makespan of the solution
+        dataset : objet dataset
+            Ensemble de données contenant les informations sur les tâches et les machines.
+        solution : tableau binaire de forme (n_tasks, n_machines)
+            Matrice binaire indiquant l'affectation des tâches aux machines.
+    Returns: 
+        Makespan de la solution.
     """
     makespan = 0
     for task in dataset.tasks:
@@ -38,103 +54,116 @@ def f_makespan(dataset, solution):
                 makespan = max(makespan, task.end_time)
     return makespan
 
-def check_feasibility(dataset, solution):
-    """Check if the solution is feasible.
-    Args:
-        dataset: dataset object
-        solution: binary array of shape (n_tasks, n_machines)
-    Returns: True if the solution is feasible, False otherwise
-    """
-    # each task is assigned to exactly one machine
-    if not np.all(np.sum(solution, axis=1) == 1):
-        print("Error! Each task must be assigned to exactly one machine")
-        return False
-    # # each machine can execute one task at a time
-    # if ???:
-    #     return False
-    # # a task can only be executed if its predecessors are completed
-    # if ???:
-    #     return False
-    return True
 
 def schedule(dataset, solution):
-    """Schedule tasks on machines.
+    """Planifie les tâches sur les machines.
     Args:
-        dataset: dataset object
-        solution: binary array of shape (n_tasks, n_machines)
-    Returns: schedule: list of tuples (task_id, machine_id, start_time, end_time)
+        dataset : objet dataset
+            Contient les informations sur les tâches et les machines.
+        solution : tableau binaire de forme (n_tasks, n_machines)
+            Matrice binaire indiquant l'affectation des tâches aux machines.
+    Returns:
+        0
     """
-    # Check dimensions
-    if solution.shape != (dataset.n_tasks, dataset.n_machines):
-        raise ValueError("Invalid solution shape")
     # Schedule tasks
     for task in dataset.tasks:
         for machine in dataset.machines:
             if solution[task.id, machine.id] == 1:
                 execute_task(dataset, task, machine)
+    return 0
 
 def execute_task(dataset, task, machine):
-    """Assign a task to a machine.
+    """Exécute une tâche sur une machine.
     Args:
-        task: task object
-        machine: machine object
+        dataset : objet dataset
+            Contient les informations sur les tâches et les machines.
+        task : objet tâche
+            La tâche à exécuter.
+        machine : objet machine
+            La machine sur laquelle exécuter la tâche.
+    Returns:
+        0
     """
+
+    def get_start_time(dataset, task, machine):
+
+        predecessors = [dataset.get_task_by_id(id) for id in task.parents_id]
+        pred_machines = [p.is_assigned for p in predecessors]
+        end_times = [p.end_time for p in predecessors]
+
+        bandwidths = [dataset.bandwidth[id, machine.id] for id in pred_machines]
+        data_volumes = [dataset.data_volume[p, task.id] for p in task.parents_id]
+
+        # get end time of predecessors
+        pred_end = 0
+        for i in range(len(predecessors)):
+            if bandwidths[i] == 0:
+                transfer_time = 0
+            else:
+                transfer_time = data_volumes[i] / bandwidths[i]
+            pred_end = max(pred_end, end_times[i] + transfer_time)
+
+        # get start time
+        start_time = max(pred_end, machine.end_time)
+        return start_time
+
+
     # assign task to machine
     task.is_assigned = machine.id
-    # get predecessors
-    predecessors = [dataset.get_task_by_id(id) for id in task.parents_id]
-    # get communication time
-    pred_machines = [p.is_assigned for p in predecessors]
-    bandwidths = [dataset.bandwidth[id, machine.id] for id in pred_machines]
-    data_volumes = [dataset.data_volume[p, task.id] for p in task.parents_id]
-    end_times = [p.end_time for p in predecessors]
-    # get end time of predecessors
-    pred_end = 0
-    for i in range(len(predecessors)):
-        if bandwidths[i] == 0:
-            transfer_time = 0
-        else:
-            transfer_time = data_volumes[i] / bandwidths[i]
-        pred_end = max(pred_end, end_times[i] + transfer_time)
-    # get start
-    start_time = max(pred_end, machine.end_time)
-    # get end time
+
+    # get start and end time
+    start_time = get_start_time(dataset, task, machine)
     end_time = start_time + task.n_instructions / machine.cpu_mips
+    
     # update task and machine end time
-    task.is_done = True # TODO: is this necessary?
     task.start_time = start_time
     task.end_time = end_time
     machine.end_time = end_time
+    return 0
 
 def compute_metrics(dataset, solution):
-    """Schedule tasks on machines.
+    """Planifie les tâches sur les machines et calcule les métriques de QoS.
     Args:
-        solution: binary array of shape (n_tasks, n_machines)
-    Returns: (makespan, cost): QoS metrics
+        dataset : objet dataset
+            Contient les informations sur les tâches et les machines.
+        solution : tableau binaire de forme (n_tasks, n_machines)
+            Matrice binaire indiquant l'affectation des tâches aux machines.
+    Returns:
+        Métriques de Qualité de Service (QoS).
     """
+    def check_constraint(solution):
+        # each task is assigned to exactly one machine
+        if not np.all(np.sum(solution, axis=1) == 1):
+            print("Error! Each task must be assigned to exactly one machine")
+            return False
+        return True
+    
     # Check dimensions
     if solution.shape != (dataset.n_tasks, dataset.n_machines):
         raise ValueError("Invalid solution shape")
+    
     # Check feasibility
-    if not check_feasibility(dataset, solution):
+    if not check_constraint(solution):
         return np.inf, np.inf
+    
     # Schedule tasks
     schedule(dataset, solution)
+
     # Compute QoS metrics
     makespan = f_makespan(dataset, solution)
     cost = f_cost(dataset, solution)
-
     return makespan, cost
 
 
-# from dataset import Dataset
-# # test the functions with mock solution
+# # Test functions
+# from data.dataset import Dataset
 # dataset = Dataset(n_machines=5, n_tasks=10)
 # solution = np.zeros((dataset.n_tasks, dataset.n_machines))
-# # make sure the solution is feasible
+
 # for line in solution:
 #     line[np.random.randint(0, dataset.n_machines)] = 1
-# print(solution)
+
 # makespan, cost = compute_metrics(dataset, solution)
+# print("Solution:", solution)
 # print("Makespan:", makespan)
 # print("Cost:", cost)
