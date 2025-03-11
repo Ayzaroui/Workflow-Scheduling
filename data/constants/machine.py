ad5e8db96972da2e7a5f77931a0cbdb9a83786fd
Ayzaroui <zarouiayyo@cy-tech.fr>
2025-03-11 13:24:13 +0100

    update machine class structure

--- data/constants/machine.py
+++ data/constants/machine.py
@@ -1,5 +1,18 @@
 class Machine(object):
-    def __init__(self, bandwidth, ram, ram_cost, n_cpus, cpu_cost, cpu_mips, bandwidth_cost, is_cloud, distance, is_available=True):
+    _id_counter = 0
+    def __init__(self, bandwidth, cpu_cost, cpu_mips, bandwidth_cost, is_cloud, is_available=True, start_use=0.0, use_time=0.0, used_once=False):
+        Machine._id_counter += 1
+
+        self.bandwidth = bandwidth
+        self.cpu_cost = cpu_cost
+        self.cpu_mips = cpu_mips
+        self.bandwidth_cost = bandwidth_cost
+        self.is_cloud = is_cloud
+        self.is_available = is_available
+        self.start_use = start_use
+        self.use_time = use_time
+        self.used_once = used_once
+    
         """
         bandwidth: int -> bandwidth in Mbps
         ram: int -> RAM in Go
@@ -11,19 +24,8 @@ class Machine(object):
         distance: int -> distance in km
         is_cloud: bool -> whether the machine is in the cloud or not
         is_available: bool -> whether the machine is available or not
+        start_use: float -> start use time in seconds
+        use_time: float -> use time in seconds
+        used_once: bool -> whether the machine has been used once or not
         """
-        
-        self.bandwidth = bandwidth
-        self.ram = ram
-        self.ram_cost = ram_cost
-        self.n_cpus = n_cpus
-        self.cpu_cost = cpu_cost
-        self.cpu_mips = cpu_mips
-        self.bandwidth_cost = bandwidth_cost
-        self.distance = distance
-        self.is_cloud = is_cloud
-        self.is_available = is_available
-    
-    def generate_exec_cost(self):
-        return self.ram_cost * self.ram + self.cpu_cost * self.n_cpus
     
